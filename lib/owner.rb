8a5fb494ac5e91458287b04f00cb115893edff5e
Ryan Hollins <ballbaroi@gmail.com>
2020-03-14 15:15:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,7 @@ class Owner
       cat.mood = "nervous"
     end
     
-    self.dogs.each. do |dog|
+    self.dogs.each do |dog|
       self.dogs.delete(dog)
       dog.owner = nil
       dog.mood = "nervous"
