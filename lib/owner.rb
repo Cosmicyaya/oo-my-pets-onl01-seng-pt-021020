e57e2c39392f2cbe959b51826a5c4bc107b98cee
Ryan Hollins <ballbaroi@gmail.com>
2020-03-14 14:24:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
+ require 'pry'
+
 class Owner
-  # code goes here
+  @@all= []
+  attr_accessor :pets, :dog, :cat
+  attr_reader :species, :name
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  def self.count
+    @@all.size
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dog
+    Dog.all.select {|dog| dog.owner == self}
+  end
 end
