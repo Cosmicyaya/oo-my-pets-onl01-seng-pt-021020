d0f74afb1c3bbb6811ba26cc81896b7bf15eb30d
Ryan Hollins <ballbaroi@gmail.com>
2020-03-14 15:19:49 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
+ require 'pry'
+
 class Owner
-  # code goes here
+  @@all= []
+  attr_accessor :pets, :dog, :cat
+  attr_reader :species, :name
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  def self.count
+    @@all.size
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    
+    self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+    
+    def list_pets
+      number_of_cats = self.cats.count
+      number_of_dogs = self.dogs.count
+      
+      "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+    end
+  end
 end
