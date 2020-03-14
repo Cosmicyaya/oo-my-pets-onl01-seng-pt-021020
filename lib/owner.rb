a699ae7387675eca6fef5480d37bdd6bdb3c0a67
Ryan Hollins <ballbaroi@gmail.com>
2020-03-14 14:46:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,13 +33,4 @@ class Owner
   def dogs
     Dog.all.select {|dog| dog.owner == self}
   end
-  
-  def buy_cat(name)
-    cat = Cat.new(name)
-    @pets[:cats] << cat
-  end
-  
-  def cats
-    @pets[:cats]
-  end
 end
